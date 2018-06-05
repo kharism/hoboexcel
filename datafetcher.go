bc9dceae1fd7cd90e7e1ae0a0599a7594733ad98
kharism <greyroot00@gmail.com>
2018-06-05 11:31:28 +0700

    add adaptation to allow multiple sheet export -add interface for multiplesheet handling -separate concern from exportWorksheet function so it does not handle creating/closing sharedstrings.xml -update test export

--- datafetcher.go
+++ datafetcher.go
@@ -7,5 +7,13 @@ type RowFetcher interface {
 
 // Fetch next sheet, if no more row exists return nil
 type SheetFetcher interface {
-	NextSheet() XlsxSheet
+	NextSheet() Sheet
+	GetSheetNames() []string
+}
+type SheetNamer interface {
+	GetSheetName() string
+}
+type Sheet interface {
+	RowFetcher
+	SheetNamer
 }
